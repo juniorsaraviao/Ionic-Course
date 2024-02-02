ed4e268c1e2e383fdc21c443745bdabbf1a7f241
Junior Saravia <junior.saravia.o@uni.pe>
2024-02-02 09:28:53 -0500

    Angular training

--- 02-angular-bases/src/app/app.component.ts
+++ 02-angular-bases/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./pages/about/about.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AboutComponent, ContactComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-bases';
