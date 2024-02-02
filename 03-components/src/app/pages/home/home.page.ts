516a6e3eb096d33ff9be1e255962c7c2807e3d1f
Junior Saravia <junior.saravia.o@uni.pe>
2024-02-02 11:45:36 -0500

    Add ion-list

--- 03-components/src/app/pages/home/home.page.ts
+++ 03-components/src/app/pages/home/home.page.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+interface CustomComponent {
+  icon: string,
+  name: string,
+  redirectTo: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage  {
+
+  components: CustomComponent[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
 
   constructor() { }
 }
