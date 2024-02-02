4d57460cbdb44d373f3117809b250bf5cac6a979
Junior Saravia <junior.saravia.o@uni.pe>
2024-02-02 09:06:27 -0500

    Use debbuger

--- 01-my-app/src/app/tab1/tab1.page.ts
+++ 01-my-app/src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+
+    let num = 10;
+
+    //debugger; Use debugger on browsers
+
+    num = 20;
+
+    num += 10;
+
+    num = 0;
+
+    console.log(num)
+  }
 
 }
